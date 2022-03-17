db059b440d6389f14e1b567f20bba5f3fbd0b329
imajs7 <anukhg@gmail.com>
2022-03-17 12:20:26 +0530

    assignment 2 completed

--- src/com/greatlearning/main/Main.java
+++ src/com/greatlearning/main/Main.java
@@ -1,9 +1,39 @@
 package com.greatlearning.main;
 
+import com.greatlearning.departments.AdminDepartment;
+import com.greatlearning.departments.HrDepartment;
+import com.greatlearning.departments.TechDepartment;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		AdminDepartment admin = new AdminDepartment();
+		HrDepartment hr = new HrDepartment();
+		TechDepartment tech = new TechDepartment();
+		
+		System.out.println( admin.departmentName() );
+		System.out.println( admin.getTodaysWork() );
+		System.out.println( admin.getWorkDeadline() );
+		System.out.println( admin.isTodayAHoliday() );
+		
+		System.out.println();
+		
+		System.out.println( hr.departmentName() );
+		System.out.println( hr.doActivity() );
+		System.out.println( hr.getTodaysWork() );
+		System.out.println( hr.getWorkDeadline() );
+		System.out.println( hr.isTodayAHoliday() );
+		
+		System.out.println();
+		
+		System.out.println( tech.departmentName() );
+		System.out.println( tech.getTodaysWork() );
+		System.out.println( tech.getWorkDeadline() );
+		System.out.println( tech.getTechStackInformation() );
+		System.out.println( tech.isTodayAHoliday() );
+		
 
 	}
 
