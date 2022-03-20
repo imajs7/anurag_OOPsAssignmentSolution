21ff7931275eed4e8502050c291fe7b6ec99974a
imajs7 <anukhg@gmail.com>
2022-03-20 23:17:09 +0530

    Added greeting message

--- src/com/greatlearning/main/Main.java
+++ src/com/greatlearning/main/Main.java
@@ -9,18 +9,20 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		String greetings = "Welcome to ";
+		
 		AdminDepartment admin = new AdminDepartment();
 		HrDepartment hr = new HrDepartment();
 		TechDepartment tech = new TechDepartment();
 		
-		System.out.println( admin.departmentName() );
+		System.out.println( greetings + admin.departmentName() );
 		System.out.println( admin.getTodaysWork() );
 		System.out.println( admin.getWorkDeadline() );
 		System.out.println( admin.isTodayAHoliday() );
 		
 		System.out.println();
 		
-		System.out.println( hr.departmentName() );
+		System.out.println( greetings + hr.departmentName() );
 		System.out.println( hr.doActivity() );
 		System.out.println( hr.getTodaysWork() );
 		System.out.println( hr.getWorkDeadline() );
@@ -28,7 +30,7 @@ public class Main {
 		
 		System.out.println();
 		
-		System.out.println( tech.departmentName() );
+		System.out.println( greetings + tech.departmentName() );
 		System.out.println( tech.getTodaysWork() );
 		System.out.println( tech.getWorkDeadline() );
 		System.out.println( tech.getTechStackInformation() );
